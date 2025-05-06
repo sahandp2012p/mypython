d6cfa4fec001c74cde8dc8beca591a6ec72e53d3
Sahand <spourjavad2012@gmail.com>
2025-05-05 17:06:54 -0700

    Value of code in run button

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,11 +1,14 @@
 import { Editor } from "@monaco-editor/react";
 import { Button } from "./ui/button";
 import { Play } from "lucide-react";
+import { useState } from "react";
 
 export default () => {
+  const [code, setCode] = useState();
+
   return (
     <>
-      <Button className="my-[2em]">
+      <Button className="my-[2em]" onClick={() => console.log(code)}>
         Run <Play />
       </Button>
 
@@ -17,6 +20,8 @@ export default () => {
           fontSize: 16,
           formatOnType: true,
         }}
+        value={code}
+        onChange={(newCode) => setCode(newCode)}
       />
     </>
   );
